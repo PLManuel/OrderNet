b22a227462917f7dda2fff12d522d6e640f6d960
PLManuel <manuel715pl@gmail.com>
2025-07-15 20:33:15 -0500

    añade https en localhost

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,18 @@ import tailwindcss from "@tailwindcss/vite"
 
 import react from "@astrojs/react"
 
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel"
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      https: {
+        cert: './localhost.pem',
+        key: './localhost-key.pem',
+      },
+    },
   },
   server: {
     port: 4322,
